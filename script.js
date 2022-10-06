1a9f4302be9b224b14c13fb69e9e6fa0631d50dd
kusum chowdhury <you@example.com>
2022-10-06 20:59:36 +0530

    created board

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
 document.addEventListener("DOMContentLoaded", function() {
-  createDivs(16);
+  createDivs(200);
 })
- 
+ let size;
 function createDivs(size) {
   let container = document.querySelector(".container");
-   container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
-   container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+ container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
+ container.style.gridTemplateRows = `repeat(${size}, 1fr)`;
+ let divsNum = size * size;
+ for(let i = 0; i < divsNum; i++) {
 
+  let div = document.createElement('div');
+
+  div.addEventListener('mouseover', function(){
+    div.style.background = "black";
+  })  
+  container.insertAdjacentElement("beforeend", div);
+ }
 }
-
+function getVal() {
+  let val = document.querySelector('input').value;
+  console.log(val);
+}
