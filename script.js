f9310a01c1e4856b1eeca76efdb00d3d13b86f95
kusum chowdhury <kusumchowdhury5832@gmail.com>
2022-11-10 03:39:03 +0530

    improved UI

--- script.js
+++ script.js
@@ -6,36 +6,48 @@ let div = document.querySelector('div');
 let black = document.querySelector('#black-color');
 let rgb = ['red', 'green', 'blue', 'yellow', 'pink', 'purple', 'royaleblue', 'lightblue', 'coral', 'black'];
 
+// select random color fro rgb variable
 function selectRandomColor() {
   let randomIndex = Math.floor(Math.random() * rgb.length)
   return rgb[randomIndex];
 }
 
+// add on class on clicking of rainbow button
 rgbColor.addEventListener('click', () => {
   rgbColor.classList.add("on");
-  black.classList.remove("on");
 })
+
+//assign white color to color variable
+//uses white to remove other colors
 eraser.addEventListener("click", ()=> {
   color = "white";
+  //remove on class from rainbow button 
   rgbColor.classList.remove("on")
 })
+
+//change back color variable to black 
 black.addEventListener('click', ()=> {
   color = "black";
+  //removes on class from rainbow button
   rgbColor.classList.remove("on")
 })
 
 
 
-
+//ask for input from user in prompt
+//asigns input to the size of grid(board)
 function askforInput() {
-  let size = prompt("enter num"); 
+  let size = prompt("enter size of the grid"); 
   createDivs(size)
 
 }
+
+//on domloaded provides default grid
 document.addEventListener("DOMContentLoaded", function() {
   createDivs(sizeOfGrid);
 })
 
+//create board with grid
 function createDivs(size) {
   let container = document.querySelector(".container");
  container.style.gridTemplateColumns = `repeat(${size}, 1fr)`;
